e19a364afc186d9a8b04cf3dc6de75a791dd1b42
Matheus Ferreira <matheusfgsantos31@gmail.com>
2025-01-23 19:14:10 -0300

    Restruturação das pastas ára receber a versão 2

---
+++ src/main/java/TelaInicial.java
@@ -0,0 +1,82 @@
+package src.main.java;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import src.main.java.telas.Mitologias;
+import src.main.java.telas.Sobre;
+
+public class TelaInicial extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("FOLCLORE SHARE");
+
+        BorderPane folclore = new BorderPane();
+
+        VBox vbox = new VBox(10);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setPadding(new Insets(20));
+
+        Label tituloLabel = new Label("SEJA BEM VINDO(A)");
+        tituloLabel.setId("titulo-label");
+        vbox.getChildren().add(tituloLabel);
+
+        Region space1 = new Region();
+        space1.setPrefHeight(100);
+        vbox.getChildren().add(space1);
+
+        Button iniciar = new Button("INICIAR");
+        iniciar.setId("iniciar-button");
+        vbox.getChildren().add(iniciar);
+
+        iniciar.setOnAction(e -> {
+            Stage mitologiasStage = new Stage();
+            Mitologias mitologias = new Mitologias();
+            mitologias.start(mitologiasStage);
+            primaryStage.close();
+        });
+
+        Button sobre = new Button("SOBRE NÓS");
+        sobre.setId("sobre-button");
+        vbox.getChildren().add(sobre);
+
+        sobre.setOnAction(e -> {
+            Stage sobreStage = new Stage();
+            Sobre telasobre = new Sobre();
+            telasobre.start(sobreStage);
+            primaryStage.close();
+        });
+
+        folclore.setCenter(vbox);
+
+        HBox rodape = new HBox(10);
+        rodape.setAlignment(Pos.CENTER);
+        rodape.setPadding(new Insets(10));
+        rodape.setId("rodape");
+
+        Label rodapeLabel = new Label("By Matheus Ferreira");
+        rodapeLabel.setId("rodape-label");
+
+        rodape.getChildren().addAll(rodapeLabel);
+        folclore.setBottom(rodape);
+
+        Scene mainScene = new Scene(folclore, 900, 540);
+        mainScene.getStylesheets().add("src/main/resources/Styles/TelaInicial.css");
+
+        primaryStage.setScene(mainScene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/telas/persons/Iara.java
@@ -0,0 +1,92 @@
+package src.main.java.telas.persons;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import src.main.java.telas.Mitologias;
+
+public class Iara extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("FOLCLORE SHARE");
+        primaryStage.getIcons().add(new Image(getClass().getResource("/Imagens/LogoJanela.jpg").toExternalForm()));
+
+        BorderPane folclore = new BorderPane();
+        folclore.setStyle("-fx-background-image: url('file:C:/Users/matheus.fgs/Desktop/FOLCLORE/Imagens/FlorestBackground.gif');" +
+                "-fx-background-size: cover;");
+
+        VBox vbox = new VBox(10);
+        vbox.setPadding(new Insets(20));
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setId("VBOX");
+
+        Label tituloLabel = new Label("IARA");
+        tituloLabel.setId("titulo-label-iara");
+        vbox.getChildren().addAll(tituloLabel);
+
+        Image curupiraImage = new Image("file:C:/Users/matheus.fgs/Desktop/FOLCLORE/src.main.resources.Imagens/Personagens/src.main.java.telas.persons.Iara.png");
+        ImageView curupiraImageView = new ImageView(curupiraImage);
+        curupiraImageView.setFitWidth(100);
+        curupiraImageView.setFitHeight(100);
+        curupiraImageView.setPreserveRatio(true);
+
+        StackPane curupiraContainer = new StackPane();
+        curupiraContainer.getChildren().add(curupiraImageView);
+        curupiraContainer.setId("iara-image");
+        curupiraContainer.setPadding(new Insets(10));
+
+        vbox.getChildren().addAll(curupiraContainer);
+
+        Label historia = new Label(
+                "A src.main.java.telas.persons.Iara, também conhecida como Mãe d'Água, é uma das mais encantadoras figuras do folclore brasileiro. Ela é uma\n" +
+                        "serena e bela sereia que vive nas águas doces dos rios e lagos da Amazônia. Sua aparência é descrita como a de uma\n" +
+                        "mulher de longos cabelos negros e olhos penetrantes, com metade do corpo humano e a outra metade de peixe. src.main.java.telas.persons.Iara é\n" +
+                        "famosa por seu canto hipnotizante, que atrai os navegantes para as profundezas das águas. Aqueles que ouvem sua voz\n" +
+                        "melodiosa não conseguem resistir ao seu encanto e acabam sendo levados para o fundo dos rios. Apesar de sua beleza\n" +
+                        "e fascinação, a src.main.java.telas.persons.Iara pode ser perigosa para aqueles que se aproximam de seu território sem respeito. No entanto,\n" +
+                        "algumas histórias contam que a src.main.java.telas.persons.Iara é uma protetora das águas e das criaturas que nela habitam, punindo aqueles\n" +
+                        "que poluem ou ameaçam seu ambiente. Assim, ela é uma representação da força misteriosa da natureza e da importância\n" +
+                        "de preservar os rios e as florestas do Brasil."
+        );
+        historia.setId("historia");
+        vbox.getChildren().add(historia);
+
+        folclore.setCenter(vbox);
+
+        Button sair = new Button("SAIR");
+        sair.setId("sair-button");
+        sair.setOnAction(e -> {
+            Stage mitologiasStage = new Stage();
+            Mitologias mitologias = new Mitologias();
+            mitologias.start(mitologiasStage);
+            primaryStage.close();
+        });
+
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER);
+        hBox.setPadding(new Insets(10));
+        hBox.getChildren().add(sair);
+
+        folclore.setBottom(hBox);
+
+        Scene scene = new Scene(folclore, 900, 540);
+        scene.getStylesheets().add("/Styles/Historias.css");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
